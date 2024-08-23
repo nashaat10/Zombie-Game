832ccd5f85358b0a9e3f69e7a22c43b368ce99b7
nashaat10 <94199891+nashaat10@users.noreply.github.com>
2024-08-23 05:06:20 +0300

    test

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 import express, { json } from "express";
 import cors from "cors";
+// import  Pool  from "./db.js";
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+const corsOptions = { origin: process.env.URL || "*" };
+
+app.use(cors(corsOptions)); // Enable CORS for all routes (or specify a route) that your server should accept requests from
+app.use(json());
