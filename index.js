6b2c89c5d85620f7378a515da4ec4238707d004b
nashaat10 <94199891+nashaat10@users.noreply.github.com>
2024-08-27 21:50:20 +0300

    up

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 import express, { json } from "express";
 import cors from "cors";
-// import  Pool  from "./db.js";
-
+// import Pool from "./db";
 const app = express();
 const PORT = process.env.PORT || 5000;
 const corsOptions = { origin: process.env.URL || "*" };
 
 app.use(cors(corsOptions)); // Enable CORS  for all routes (or specify a route) that your server should accept requests from
 app.use(json());
+
+app.get("/leaderboards", (req, res) => {
+  res.json("hello");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
